92dcf4c7046fc995a059ee87f8bea76ef0cdb298
Kishan <bang.siki2009@gmail.com>
2022-08-10 19:14:40 +0545

    Added code to fetch all todos and context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,32 @@
+import { useContext, useEffect } from 'react';
+import { TodoContext } from '../context/TodoContext';
+
 import styles from './Home.module.css';
 import TodoForm from '../components/TodoForm';
 import Todo from '../components/Todo';
 
 const Home = () => {
+    const { todos, dispatch } = useContext(TodoContext);
+
+
+    useEffect(() => {
+        const getTodos = async () => {
+            const response = await fetch('/todos');
+            const todos = await response.json();
+
+            dispatch({ type: 'GET_WORKOUT', payload: todos.todos });
+        }
+        
+        getTodos();
+    }, [dispatch]);
+
+    
     return (
         <div className={styles.main}>
             <div className={styles.lists}>
-                <Todo/>
+                {todos && todos.map(todo => (
+                    <Todo key={todo._id} todo={todo} />
+                )) }
             </div>
             <div className={styles.form}>
                 <TodoForm/>
