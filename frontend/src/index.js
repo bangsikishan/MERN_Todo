0481699c46ad54e16836764a6e014a27aedbc8fe
Kishan <bang.siki2009@gmail.com>
2022-08-10 19:15:19 +0545

    Added ContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TodoContextProvider from './context/TodoContext';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <TodoContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TodoContextProvider>
 );
