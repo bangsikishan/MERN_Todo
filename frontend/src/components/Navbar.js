09f5ba888f8746ad29c2e4afde4683be5974d46f
Kishan <bang.siki2009@gmail.com>
2022-08-13 20:57:26 +0545

    Added conditional rendering to navlinks

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,35 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+    const { user, dispatch } = useContext(UserContext);
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        dispatch({ type: 'LOGOUT'});
+    }
+
     return (
         <nav className={styles.navbar}>
             <h2>
                 <Link to='/'>Todo</Link>
             </h2>
             <ul>
-                <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/signup'>Signup</Link></li>
+                {user && (
+                    <>
+                        <li>{user.email}</li>
+                        <li className={styles.logout} onClick={logout}>Logout</li>
+                    </>
+                )}
+                {!user && (
+                    <>
+                        <li><Link to='/login'>Login</Link></li>
+                        <li><Link to='/signup'>Signup</Link></li>
+                    </>
+                )}
             </ul>
         </nav>
     );
