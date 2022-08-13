b2b1bd78079da3ba15c8019acf4a8164f1f602ba
Kishan <bang.siki2009@gmail.com>
2022-08-13 18:32:36 +0545

    Added UserContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import TodoContextProvider from './context/TodoContext';
+import UserContextProvider from './context/UserContext';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TodoContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </TodoContextProvider>
+  <UserContextProvider>
+    <TodoContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </TodoContextProvider>
+  </UserContextProvider>
 );
