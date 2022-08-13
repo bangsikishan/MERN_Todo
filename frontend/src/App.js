a8d8751f479e854974b946c22ce1160b6a479d7b
Kishan <bang.siki2009@gmail.com>
2022-08-13 21:02:07 +0545

    Added conditional routing to nav route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -6,13 +8,16 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
+  const { user } = useContext(UserContext);
+
   return (
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<Signup/>} />
+        <Route exact path='/' element={user ? <Home/> : <Navigate to='/login'/>} />
+        <Route path='/login' element={!user ? <Login/> : <Navigate to='/'/>} />
+        <Route path='/signup' element={!user ? <Signup/> : <Navigate to='/'/>} />
+        <Route path='*' element={user ? <Navigate to='/'/> : <Navigate to='/login'/>} />
       </Routes>
     </BrowserRouter>
   );
