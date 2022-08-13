e4716908bbf1d4b0e36209491b9661dededc8ebb
Kishan <bang.siki2009@gmail.com>
2022-08-13 18:31:50 +0545

    Added context and error handling

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,19 +1,71 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../context/UserContext';
+
 import styles from './Signup.module.css';
 
 const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { dispatch } = useContext(UserContext);
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const emailErrorDiv = document.querySelector('#email');
+        const passwordErrorDiv = document.querySelector('#password');
+
+        emailErrorDiv.innerHTML = '';
+        passwordErrorDiv.innerHTML = '';
+
+        try {
+            const response = await fetch('/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
+
+            const data = await response.json();
+
+            if(!data.error) {
+                dispatch({ type: 'CREATE_USER', payload: data });
+
+                localStorage.setItem('user', JSON.stringify(data));
+
+                setEmail('');
+                setPassword('');
+                return;
+            }
+
+            
+            // HANDLE ERROR
+            if(data.error.includes('email')) {
+                emailErrorDiv.innerHTML = data.error;
+            }
+            if(data.error.includes('Password')) {
+                passwordErrorDiv.innerHTML = data.error;
+            }
+        }
+        catch(err) {
+            console.log(err.message);
+        }
+    }
+
+
     return (
-        <div className={styles.register}>
+        <div className={styles.register} onSubmit={handleSubmit}>
             <form>
                 <h2>Signup</h2>
                 <label>
                     <span>Email:</span>
-                    <input type="email" required />
-                    <div className={styles.error}></div>
+                    <input type="email" onChange={e => setEmail(e.target.value)} value={email} required />
+                    <div className={styles.error} id='email'></div>
                 </label>
                 <label>
                     <span>Password:</span>
-                    <input type="password" required />
-                    <div className={styles.error}></div>
+                    <input type="password" onChange={e => setPassword(e.target.value)} value={password} required />
+                    <div className={styles.error} id='password'></div>
                 </label>
                 <button>Register</button>
             </form>
