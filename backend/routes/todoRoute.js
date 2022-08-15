4ad68dfdfcdeea653dd12e5099ba0c02b1ed12ad
Kishan <bang.siki2009@gmail.com>
2022-08-15 12:32:20 +0545

    Added the validate middleware

--- backend/routes/todoRoute.js
+++ backend/routes/todoRoute.js
@@ -2,11 +2,13 @@
 const express = require('express');
 
 const todoController = require('../controllers/todoController');
+const validate = require('../authentication/validateToken');
 
 const router = express.Router();
 
 
 // CREATE ROUTES
+router.use(validate);
 router.get('/todos', todoController.getTodos);
 router.post('/todo', todoController.createTodos);
 router.delete('/:id', todoController.deleteTodo);
